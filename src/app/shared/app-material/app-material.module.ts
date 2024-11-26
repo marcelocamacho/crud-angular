6ed24d624d2f094c345a74247c023b0d6d9a1aad
Marcelo Santana Camacho <marcelo.camacho@serpro.gov.br>
2024-11-26 16:35:27 -0300

    Refatoração para organizar as importações de módulo e reduzir a quantidade de imports

---
+++ src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+@NgModule({
+  declarations: [],
+  exports:[
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
+  ]
+})
+export class AppMaterialModule { }
