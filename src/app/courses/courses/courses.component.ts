d4b2da4961185134d7812a0c75ac4dc74db89762
Marcelo Santana Camacho <marcelo.camacho@serpro.gov.br>
2024-11-26 15:54:57 -0300

    inclusão da tabela e datasource inicial

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   standalone: false,
-  
+
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
+  courses: Course[] = [
+    {_id:"1", name: 'Angular', category:'Frontend'},
+    {_id:"2", name: 'Java', category:'Banckend'}
+  ];
+  displayedColumns = ['name','category'];
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+
+  }
 }
