550029f98c67bbf567bf21de228cdd1f961ac5e0
Marcelo Santana Camacho <marcelo.camacho@serpro.gov.br>
2024-11-27 15:51:36 -0300

    ng g pipe shared/pipe/category

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorDialogComponent } from './componenents/error-dialog/error-dialog.component';
 import { AppMaterialModule } from './app-material/app-material.module';
+import { CategoryPipe } from './pipe/category.pipe';
 
 
 
 @NgModule({
   declarations: [
-    ErrorDialogComponent
+    ErrorDialogComponent,
+    CategoryPipe
   ],
   imports: [
     AppMaterialModule,
