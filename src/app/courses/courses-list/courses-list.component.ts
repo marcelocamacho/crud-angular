924387c65b33aa8648c6e47e981308fe84457633
Marcelo Santana Camacho <marcelo.camacho@serpro.gov.br>
2024-11-28 23:50:24 -0300

    refatoração e css da tabela de cursos

--- src/app/courses/courses-list/courses-list.component.ts
+++ src/app/courses/courses-list/courses-list.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses-list',
   standalone: false,
-  
+
   templateUrl: './courses-list.component.html',
   styleUrl: './courses-list.component.scss'
 })
-export class CoursesListComponent {
+export class CoursesListComponent implements OnInit{
 
+  @Input() courses: Course[] = [];
+  displayedColumns = ['name','category','actions'];
+
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+
+  }
+  onAdd(){
+    this.router.navigate(['new'],{relativeTo: this.route});
+    //this.router.navigate(['courses/new']);
+  }
 }
